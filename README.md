7e60db81957a209fae6876e6227e2b1ab7d98da6
kingscode1 <innocentuchenye@yahoo.com>
2021-06-20 13:22:41 +0100

    'message'

---
+++ README.md
@@ -0,0 +1,4 @@
+
+#project1
+
+step1: react app
---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">merit computers</a>
+      </header>
+      <main>
+        product
+      </main>
+      <footer>
+        all right reserved
+      </footer>
+    </div>
+  );
+}
+
+export default App;
